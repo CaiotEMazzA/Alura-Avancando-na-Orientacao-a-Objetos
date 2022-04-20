7cea05228e7abb76c4748597244081e07c771eab
Caio Adami <caio_mazza@hotmail.com>
2022-04-20 16:38:48 -0300

    Reaproveitando código da classe-mãe.

--- modelo.py
+++ modelo.py
@@ -1,71 +1,43 @@
 # Conceito da herança em Orientação a Objetos: quando eu quero que exista mais de uma classe com atributos/métodos
-# iguais, eu crio uma classe mais 'genérica' com tudo o que as minhas outras classes vão herdar em comum. Neste
-# código, essa classe 'genérica' é a classe 'Programa'.
+# iguais, eu crio uma classe mais 'genérica' com tudo o que as minhas outras classes vão herdar em comum. Neste código,
+# essa classe 'genérica' é a classe 'Programa'.
+
+# Atributos e métodos privados não são herdados pela classe filha. Para resolver o problema de acessar esses atributos
+# nas classes herdeiras, convencionou-se o uso de apenas um underscore. Assim, o atributo não sofre o name mangling
+# (name mangling = _nomedaclasse__atributo) e se comporta como um atributo normal, podendo ser usado em outras classes.
+# Lembrando que o underscore único ainda indica que esse atributo é 'privado'.
 
 class Programa:
     def __init__(self, nome, ano):
-        self.__nome = nome.title()
+        self._nome = nome.title()
         self.ano = ano
-        self.__likes = 0
+        self._likes = 0
 
     @property
     def likes(self):
-        return self.__likes
+        return self._likes
 
     def dar_like(self):
-        self.__likes += 1
+        self._likes += 1
 
     @property
     def nome(self):
-        return self.__nome
+        return self._nome
 
     @nome.setter
     def nome(self, novo_nome):
-        self.__nome = novo_nome.title()
+        self._nome = novo_nome.title()
 
-class Filme:
+class Filme(Programa):
     def __init__(self, nome, ano, duracao):
-        self.__nome = nome.title()
+        self._nome = nome.title()
         self.ano = ano
         self.duracao = duracao
-        self.__likes = 0
+        self._likes = 0
 
-    @property
-    def likes(self):
-        return self.__likes
-
-    def dar_like(self):
-        self.__likes += 1
-
-    @property
-    def nome(self):
-        return self.__nome
-
-    @nome.setter
-    def nome(self, novo_nome):
-        self.__nome = novo_nome.title()
-    
-class Serie:
+class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
-        self.__nome = nome.title()
+        self._nome = nome.title()
         self.ano = ano
         self.temporadas = temporadas
-        self.__likes = 0
-
-    @property
-    def likes(self):
-        return self.__likes
-
-    def dar_like(self):
-        self.__likes += 1
-
-    @property
-    def nome(self):
-        return self.__nome
-
-    @nome.setter
-    def nome(self, novo_nome):
-        self.__nome = novo_nome.title()
-
-    def dar_like(self):
-        self.likes += 1
+        self._likes = 0
