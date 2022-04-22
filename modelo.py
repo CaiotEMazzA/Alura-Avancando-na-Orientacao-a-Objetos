5d415b21c7f30ed770758d5141d77972cb96afaf
Caio Adami <caio_mazza@hotmail.com>
2022-04-22 16:38:59 -0300

    Incluindo representação textual para facilitar exibição do objeto.

--- modelo.py
+++ modelo.py
@@ -67,24 +67,24 @@ class Programa:
     def nome(self, novo_nome):
         self._nome = novo_nome.title()
 
-    def imprime(self):
-        print(f'{self._nome} - {self.ano} - {self._likes}')
+    def __str__(self):
+        return f'{self._nome} - {self.ano} - {self._likes} likes'
 
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
         super().__init__(nome, ano)
         self.duracao = duracao
 
-    def imprime(self):
-        print(f'{self._nome} - {self.ano} - {self.duracao} min - {self._likes} likes')
+    def __str__(self):
+        return f'{self._nome} - {self.ano} - {self.duracao} min - {self._likes} likes'
 
 class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
         super().__init__(nome, ano)
         self.temporadas = temporadas
 
-    def imprime(self):
-        print(f'{self._nome} - {self.ano} - {self.temporadas} temporadas - {self._likes} likes')
+    def __str__(self):
+        return f'{self._nome} - {self.ano} - {self.temporadas} temporadas - {self._likes} likes'
 
 procurando_nemo = Filme('Procurando nemo', 2003, 100)
 procurando_nemo.dar_like()
@@ -95,4 +95,4 @@ mandaloriano.dar_like()
 filmes_e_series = [procurando_nemo, mandaloriano]
 
 for programa in filmes_e_series:
-    programa.imprime()
+    print(programa)
